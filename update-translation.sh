f7d1de9d46d2b9ae3e8206381b5f5504b3a9c1ae
NitrogenCND <147336685+NitrogenCND@users.noreply.github.com>
2023-10-22 18:09:11 +0200

    Delete update-translation.sh

--- update-translation.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-xgettext --from-code UTF-8 -o locale/le-chat-php.pot `find . -iname '*.php'`
-for translation in `find locale -iname '*.po'`; do msgmerge -U "$translation" locale/le-chat-php.pot; msgfmt -o ${translation:0:-2}mo "$translation"; done
